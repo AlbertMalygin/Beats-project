849cc84947d21c3f18da5170053bf6df2ee98531
Albert Malygin <albert.malygin@gmail.com>
2023-08-09 19:38:19 +0500

    added functional to team and shop sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
+import {SHOP_BASE} from "./shop_items.js";
+
+const tabletSize = 768;
+const MobileSize = 480;
+
 const hamburgerBtn = document.querySelector('.hamburger__checkbox');
 const modalWindow = document.querySelector('.modal');
+const screenWidth = window.innerWidth;
+
 
 hamburgerBtn.addEventListener('click', () => {
   if(hamburgerBtn.checked) {
@@ -19,3 +26,114 @@ window.addEventListener('scroll', () => {
   }
 });
 
+
+
+//Shop variables
+const arrowLeft = document.querySelector('.slider-controls__arrow--left');
+const arrowRight = document.querySelector('.slider-controls__arrow--right');
+const item = {
+  shopItemImg: document.querySelector('.shop__img'),
+  dropMenu: document.querySelector('.dropdown-menu__list'),
+  shopHeader: document.querySelector('.shop__header'),
+  shopDescription: document.querySelector('.shop__desc'),
+  itemProperties: document.querySelector('.item-prop'),
+  itemPrice: document.querySelector('.shop-order__price'),
+  orderBtn: document.querySelector('.order-btn')
+};
+let itemNum = 0;
+item.shopItemImg.src = SHOP_BASE[0].imgUrlDesktop;
+
+//Shop slider functional
+let addSliderContent = () => {
+  item.shopItemImg.src = SHOP_BASE[itemNum].imgUrlDesktop;
+  item.dropMenu.childNodes[1].childNodes[3].innerHTML = SHOP_BASE[itemNum].bluetoothVersion;
+  item.dropMenu.childNodes[3].childNodes[3].innerHTML = SHOP_BASE[itemNum].range;
+  item.dropMenu.childNodes[5].childNodes[3].innerHTML = SHOP_BASE[itemNum].charger;
+  item.dropMenu.childNodes[7].childNodes[3].innerHTML = SHOP_BASE[itemNum].weight;
+  item.dropMenu.childNodes[9].childNodes[3].innerHTML = SHOP_BASE[itemNum].guarantee;
+  item.shopHeader.innerHTML = SHOP_BASE[itemNum].name;
+  item.shopDescription.innerHTML = SHOP_BASE[itemNum].itemDescription;
+  item.itemProperties.childNodes[1].childNodes[3].innerHTML = SHOP_BASE[itemNum].charger;
+  item.itemProperties.childNodes[3].childNodes[3].innerHTML = SHOP_BASE[itemNum].weight + 'амм';
+  item.itemPrice.innerHTML= SHOP_BASE[itemNum].price;
+  item.orderBtn.href = SHOP_BASE[itemNum].orderBtnUrl;
+};
+
+arrowLeft.addEventListener('click', () => {
+  itemNum--;
+  if (itemNum < 0) {
+    itemNum = SHOP_BASE.length - 1;
+  }
+
+  addSliderContent();
+});
+
+arrowRight.addEventListener('click', () => {
+  itemNum++;
+  if (itemNum >= SHOP_BASE.length) {
+    itemNum = 0;
+  }
+  
+  addSliderContent();
+});
+
+
+
+//Team section functional
+const imgUrls = [
+                  './img/employee/boris.png',
+                  './img/employee/alla.png',
+                  './img/employee/kirill.png',
+                  './img/employee/mila.png'
+                ];                
+const employeePhoto = document.querySelectorAll('.employee__photo');
+const employeeTabPhoto = document.querySelectorAll('.employee__tab-photo');
+const teamContent = document.querySelectorAll('.employee__content');
+const contentControl = document.querySelectorAll('.employee__control');
+
+let addPhotoUrl = (photosArr, photosUrl, photosUrlForRemove) => {
+  for (let i = 0; i < photosArr.length; i++) {
+    photosArr[i].src = photosUrl[i];
+    photosUrlForRemove[i].src = '';
+  }
+}
+
+if (screenWidth <= tabletSize) {
+  addPhotoUrl(employeeTabPhoto, imgUrls, employeePhoto);
+} else {
+  addPhotoUrl(employeePhoto, imgUrls, employeeTabPhoto);
+}
+
+for (let i = 0; i < contentControl.length; i++) {
+  contentControl[i].addEventListener('click', () => {
+    if (teamContent[i].classList.contains('employee__content--active')) {
+      teamContent[i].classList.remove('employee__content--active');
+    } else {
+      for (let n = 0; n < contentControl.length; n++) {
+        teamContent[n].classList.remove('employee__content--active');
+      }
+      teamContent[i].classList.add('employee__content--active');
+    }
+    
+  });
+}
+
+
+
+
+
+
+//Window resize events
+window.addEventListener('resize', () => {
+
+  //Add photo urls to team section
+  let width = window.innerWidth;
+
+  if (width <= tabletSize && !employeeTabPhoto[0].src.endsWith('.png')) {
+    console.log('tab');
+    addPhotoUrl(employeeTabPhoto, imgUrls, employeePhoto);
+  } else if (width > tabletSize && !employeePhoto[0].src.endsWith('.png')){
+    addPhotoUrl(employeePhoto, imgUrls, employeeTabPhoto);
+    console.log('desktop');
+  }
+});
---
+++ scripts/shop_items.js
@@ -0,0 +1,26 @@
+export const SHOP_BASE = [
+  {
+    name: "beats solo 3. цвет «атласное серебро»",
+    imgUrlDesktop: "./img/shop-items/solo-3.png",
+    itemDescription: "Беспроводные наушники Beats Solo3 готовы к работе в любой момент. Включите их и поднесите к своему iPhone — они мгновенно подключатся к нему.",
+    price: "14 990 ₽",
+    orderBtnUrl: '#заказать.solo.3',
+    bluetoothVersion: "4.0",
+    range: "12 м",
+    charger: "3 ч",
+    weight: "215 гр",
+    guarantee: "1 год"
+  },
+  {
+    name: "beats solo pro. цвет «красный цитрус»",
+    imgUrlDesktop: "./img/shop-items/solo-pro.png",
+    itemDescription: "Беспроводные наушники Solo Pro помогают настроиться на творчество. Для этого предусмотрено два режима прослушивания: режим с активным шумоподавлением и Прозрачный режим.",
+    price: "14 490 ₽",
+    orderBtnUrl: '#заказать.solo.pro',
+    bluetoothVersion: "4.0",
+    range: "10 м",
+    charger: "2 ч",
+    weight: "215 гр",
+    guarantee: "1 год"
+  }
+];
